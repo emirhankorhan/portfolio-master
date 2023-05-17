b02995c46f7c11540162ed2ecf9901c153916109
emirhankorhan <korhanemirhann@gmail.com>
2023-05-17 15:53:38 +0300

    three

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./home.css"
+import spill from "./spill.svg"
 
 function Home() {
   return (
     <div className='fullScreen'>
-      <div className='textdiv'></div>
-      <div className='imgdiv'></div>
+      <div className='textdiv'>
+        <h2>Merhaba, ben Emirhan Körhan</h2>
+        <p>Front-End Web Geliştiricisiyim ve <br></br>Grafik Tasarım üzerinde çalışıyorum.</p>
+      </div>
+      <div className='imgdiv'><img src={spill}/></div>
     </div>
   )
 }
