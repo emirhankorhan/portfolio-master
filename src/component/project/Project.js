c8f6304ec0c1c8703f704244d70e6255bf57b21d
emirhankorhan <korhanemirhann@gmail.com>
2023-05-17 18:32:58 +0300

    seco

--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -3,7 +3,25 @@ import "./project.css"
 
 function Project() {
   return (
-    <div className='projectdiv'>Project</div>
+    <div className='projectdiv'>
+        <div className='projectbos'></div>
+        <div className='projectname'><h1>Project's Portfolio</h1></div>
+        <div className='projects'>
+            <div className='projecttagone'>
+                <div className='project1'></div>
+                <div className='project2'></div>
+                <div className='project3'></div>
+            </div>
+
+            <div className='projecttagtwo'>
+                <div className='project4'></div>
+                <div className='project5'></div>
+                <div className='project6'></div>
+            </div>
+            
+            
+        </div>
+    </div>
   )
 }
 
